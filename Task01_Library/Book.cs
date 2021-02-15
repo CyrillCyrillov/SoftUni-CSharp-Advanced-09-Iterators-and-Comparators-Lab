e1d3b926320d0e03bc209214ada0616db4a6ff72
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-15 22:50:24 +0200

    Add files via upload

---
+++ Task01_Library/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors.ToList();
+        }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; private set; }
+
+    }
+}
